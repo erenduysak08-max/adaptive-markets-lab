d3c8d5edd8dfdb9ce1972752665714fe32a5ea58
erenduysak08-max <Erenduysak08@gmail.com>
2026-08-31 18:11:20 +0100

    download and explore SPY market data

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-returns = pd.Series([
-    0.001,
-    0.002,
-    -0.001,
-    0.001,
-    0.000,
-    0.002,
-    0.001,
-    0.002,
-    0.050,
-    0.060,
-    0.040
-])
-fast = returns.ewm(halflife=2).mean()
-slow = returns.ewm(halflife=10).mean()
+import yfinance as yf
+data = yf.download('SPY', start='2020-01-01', end='2021-01-01', auto_adjust = False, multi_level_index = False)
+print(data.head())
+print(data.tail())
+print(data.shape)
+print(data.columns.isna().sum())
+print(data.columns)
+prices = data["Adj Close"]
+returns = (prices - prices.shift(-1))/prices
+print(returns.head(10))
+print(prices.pct_change().head(10))
+log_returns = np.log(prices/prices.shift(-1))
+print(log_returns.head(10))
 
-plt.plot(returns, label="Returns")
-plt.plot(fast, label="Fast EWM")
-plt.plot(slow, label="Slow EWM")
-plt.plot(y_values)
-plt.legend()
+plt.plot(prices, "b-")
+plt.plot(returns, "r-")
 plt.show()
