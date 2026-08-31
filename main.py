e542100e2e2f13b18b0072e291ba50a1d561fa57
erenduysak08-max <Erenduysak08@gmail.com>
2026-08-31 01:58:09 +0100

    learn pandas time series operations

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import numpy as np
-prices = np.array([100, 103, 101, 106, 108])
-print("Prices: ", prices)
-print("First price: ", prices[0])
-print("Last Price: ", prices[-1])
-print("Middle three prices: ", prices[1:4])
-print("10% Increase on prices: ", prices * 1.1)
-print("Mean: ", np.mean(prices))
-print("Standard Deviation: ", np.std(prices))
+import pandas as pd
+import matplotlib.pyplot as plt
+returns = pd.Series([
+    0.001,
+    0.002,
+    -0.001,
+    0.001,
+    0.000,
+    0.002,
+    0.001,
+    0.002,
+    0.050,
+    0.060,
+    0.040
+])
+fast = returns.ewm(halflife=2).mean()
+slow = returns.ewm(halflife=10).mean()
+
+plt.plot(returns, label="Returns")
+plt.plot(fast, label="Fast EWM")
+plt.plot(slow, label="Slow EWM")
+plt.plot(y_values)
+plt.legend()
+plt.show()
